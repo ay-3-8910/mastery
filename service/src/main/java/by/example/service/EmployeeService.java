d426178bbfce6cb409bce878c54a65f7fe0a1a40
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-06-28 12:30:34 +0300

    create fake service

--- service/src/main/java/by/example/service/EmployeeService.java
+++ service/src/main/java/by/example/service/EmployeeService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Sergey Tsynin
@@ -26,12 +27,66 @@ public class EmployeeService {
      */
     public List<Employee> getAll() {
         LOGGER.debug("Create fake list");
-        ArrayList<Employee> employees = new ArrayList<Employee>();
+        ArrayList<Employee> employees = new ArrayList<>();
         employees.add(getFakeEmployee(1));
         employees.add(getFakeEmployee(2));
         employees.add(getFakeEmployee(3));
         LOGGER.debug("Return fake list");
         return employees;
+    }
+
+    /**
+     * Get employee by Id.
+     *
+     * @param employeeId train Id.
+     * @return employee.
+     */
+    public Optional<Employee> getById(Integer employeeId) {
+        LOGGER.debug("Get employee id: {}", employeeId);
+        return Optional.empty();
+    }
+
+    /**
+     * Save new employee record.
+     *
+     * @param employee object.
+     * @return saved employee Id.
+     */
+    public Integer createEmployee(Employee employee) {
+        LOGGER.debug("Create employee");
+        return 0;
+    }
+
+    /**
+     * Update employee record in the database.
+     *
+     * @param employee object.
+     * @return number of updated employees in the database.
+     */
+    public Integer updateEmployee(Employee employee) {
+        LOGGER.debug("Update employee");
+        return 0;
+    }
+
+    /**
+     * Delete employee by Id.
+     *
+     * @param employeeId employee Id.
+     * @return number of deleted employees in the database.
+     */
+    public Integer deleteEmployee(Integer employeeId) {
+        LOGGER.debug("Delete employee");
+        return 0;
+    }
+
+    /**
+     * Get the number of employees in the database.
+     *
+     * @return the number of employees in the database.
+     */
+    public Integer getEmployeesCount() {
+        LOGGER.debug("Get employees count");
+        return 0;
     }
 
     private Employee getFakeEmployee(Integer id) {
