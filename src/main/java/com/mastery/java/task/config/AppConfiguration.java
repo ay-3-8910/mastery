e48d990d72a6571ba62943ab9c40b2722279745a
SergeyTsynin <Sergey.Tsynin@gmail.com>
2021-07-14 14:05:46 +0300

    move data source properties into file

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -2,6 +2,7 @@ package com.mastery.java.task.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -14,16 +15,28 @@ import javax.sql.DataSource;
 @Configuration
 public class AppConfiguration {
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String jdbcURl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(AppConfiguration.class);
 
     @Bean
     DataSource dataSource() {
         try {
             DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-            driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-            driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/employeedb");
-            driverManagerDataSource.setUsername("employeedb");
-            driverManagerDataSource.setPassword("fakepass");
+            driverManagerDataSource.setDriverClassName(driverClassName);
+            driverManagerDataSource.setUrl(jdbcURl);
+            driverManagerDataSource.setUsername(dbUsername);
+            driverManagerDataSource.setPassword(dbPassword);
             LOGGER.debug("DataSource was created");
             return driverManagerDataSource;
         } catch (Exception e) {
