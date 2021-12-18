b3a369a78a4ac013604298ebb0fd7e7928bf77ae
ay-3-8910 <Sergey.Tsinin@gmail.com>
2021-12-18 22:00:31 +0300

    add DAO JPA implementation

--- src/main/java/com/mastery/java/task/dao/EmployeeDaoJpa.java
+++ src/main/java/com/mastery/java/task/dao/EmployeeDaoJpa.java
@@ -1,13 +1,64 @@
 package com.mastery.java.task.dao;
 
 import com.mastery.java.task.dto.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.mastery.java.task.rest.excepton_handling.NotFoundMasteryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author Sergey Tsynin
  */
-@Repository
-public interface EmployeeDaoJpa extends JpaRepository<Employee, Integer> {
+@Component
+public class EmployeeDaoJpa implements EmployeeDao {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeDaoJpa.class);
+
+    public EmployeeDaoJpa() {
+        LOGGER.debug("Employees DAO JPA was created");
+    }
+
+    @Autowired
+    EmployeeJpaRepository employeeJpaRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeJpaRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Integer employeeId) {
+        return employeeJpaRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new NotFoundMasteryException("Employee id: " + employeeId + " was not found in database"));
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeJpaRepository.save(employee);
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        if (!employeeJpaRepository.existsById(employee.getEmployeeId())) {
+            throw new NotFoundMasteryException("Employee was not found in database");
+        }
+        return employeeJpaRepository.save(employee);
+    }
+
+    @Override
+    public void deleteEmployee(Integer id) {
+        if (!employeeJpaRepository.existsById(id)) {
+            throw new NotFoundMasteryException("Employee was not found in database");
+        }
+        employeeJpaRepository.deleteById(id);
+    }
+
+    @Override
+    public Integer getEmployeesCount() {
+        return Math.toIntExact(employeeJpaRepository.count());
+    }
 }
