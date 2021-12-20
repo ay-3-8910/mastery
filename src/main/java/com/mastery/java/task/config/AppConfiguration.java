3392a50ee67d5c6e2a755e0b1dcc452f4fdd1010
ay-3-8910 <Sergey.Tsinin@gmail.com>
2021-12-20 18:19:20 +0300

    remove datasource tails

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -5,31 +5,15 @@ import com.mastery.java.task.dao.EmployeeDaoJdbc;
 import com.mastery.java.task.dao.EmployeeDaoJpa;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 /**
  * @author Sergey Tsynin
  */
 @Configuration
 public class AppConfiguration {
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-
-    @Value("${spring.datasource.url}")
-    private String jdbcURl;
-
-    @Value("${spring.datasource.username}")
-    private String dbUsername;
-
-    @Value("${spring.datasource.password}")
-    private String dbPassword;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AppConfiguration.class);
 
