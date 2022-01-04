a3471f769f2bd8fab89a40128a6e145854abf392
ay-3-8910 <Sergey.Tsinin@gmail.com>
2022-01-04 13:49:22 +0300

    fix configuration

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -6,15 +6,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
 
 /**
  * @author Sergey Tsynin
  */
 @Configuration
-@ComponentScan(basePackages = "com.mastery.java.task")
 @EnableWebMvc
+@ComponentScan(basePackages = "com.mastery.java.task")
+@PropertySource({"classpath:sql.properties", "classpath:application.properties"})
 public class AppConfiguration {
 
     @Value("${spring.datasource.driver-class-name}")
@@ -32,23 +39,35 @@ public class AppConfiguration {
     private static final Logger LOGGER = LoggerFactory.getLogger(AppConfiguration.class);
 
     @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         LOGGER.debug("MethodValidationPostProcessor was created");
         return new MethodValidationPostProcessor();
     }
-//    @Bean
-//    DataSource dataSource() {
-//        try {
-//            DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-//            driverManagerDataSource.setDriverClassName(driverClassName);
-//            driverManagerDataSource.setUrl(jdbcURl);
-//            driverManagerDataSource.setUsername(dbUsername);
-//            driverManagerDataSource.setPassword(dbPassword);
-//            LOGGER.debug("DataSource was created");
-//            return driverManagerDataSource;
-//        } catch (Exception e) {
-//            LOGGER.error("DataSource bean cannot be created", e);
-//            return null;
-//        }
-//    }
+
+    @Bean
+    DataSource dataSource() {
+        try {
+            DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+            driverManagerDataSource.setDriverClassName(driverClassName);
+            driverManagerDataSource.setUrl(jdbcURl);
+            driverManagerDataSource.setUsername(dbUsername);
+            driverManagerDataSource.setPassword(dbPassword);
+            LOGGER.debug("DataSource was created");
+            return driverManagerDataSource;
+        } catch (Exception e) {
+            LOGGER.error("DataSource bean cannot be created", e);
+            return null;
+        }
+    }
+
+    @Bean
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        LOGGER.debug("create NamedParameterJdbcTemplate");
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
 }
